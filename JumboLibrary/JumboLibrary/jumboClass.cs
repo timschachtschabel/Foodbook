db318895e401a57bb92a8dc039fd25b306dc92cd
Joep van Diessen <joepvandiessen@gmail.com>
2019-11-26 13:45:04 +0100

    Library geupdate

--- JumboLibrary/JumboLibrary/jumboClass.cs
+++ JumboLibrary/JumboLibrary/jumboClass.cs
@@ -1,28 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using HtmlAgilityPack; //Program heeft HTMLAgilityPack als dependency, installeren via NuGet
 using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
-using HtmlAgilityPack;
 
-namespace ScraperJumbo
+
+namespace JumboLibrary
 {
-    class Program
+    public static class Jumbo
     {
-        static string[] imgList = new string[20];
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine(GetProductName("5410013117001"));
-            Console.WriteLine(GetProductPrice("5410013117001"));
-            //   Console.WriteLine(GetProductFoto("5410013117001"));
-            Console.ReadLine();
-        }
-        public static async Task<string> Productnaam(string barcode)
+        //Krijgt naam uit de titel van de website
+        private static async Task<string> Productnaam(string barcode)
         {
             var url = "https://www.jumbo.com/zoeken?SearchTerm=" + barcode;
             //var url = "https://www.jumbo.com/spa-reine-mineraalwater-koolzuurvrij-75cl/727334FLS/";
@@ -41,7 +28,8 @@ namespace ScraperJumbo
             return title;
 
         }
-        public static async Task<string> Productprijs(string barcode)
+        //Krijgt prijs vanaf website
+        private static async Task<string> Productprijs(string barcode)
         {
             var url = "https://www.jumbo.com/zoeken?SearchTerm=" + barcode;
             //var url = "https://www.jumbo.com/spa-reine-mineraalwater-koolzuurvrij-75cl/727334FLS/";
@@ -52,22 +40,23 @@ namespace ScraperJumbo
             var htmlDocument = new HtmlDocument();
             htmlDocument.LoadHtml(html);
             string prijs = (from x in htmlDocument.DocumentNode.Descendants()
-                        where x.Name == "span" && x.Attributes.Contains("class")
-                        where x.Attributes["class"].Value == "jum-price-format"
+                            where x.Name == "span" && x.Attributes.Contains("class")
+                            where x.Attributes["class"].Value == "jum-price-format"
                             select x.InnerText).FirstOrDefault();
 
             return prijs;
         }
 
-        
 
-        public static string GetProductPrice (string barcode)
+        //Callt de task en geeft het resultaat terug
+        public static string GetProductPrice(string barcode)
         {
             return Task.Run(() => Productprijs(barcode)).Result;
         }
-        public static string GetProductName(string barcode)
-        {
 
+        //Callt de task en geeft het resultaat terug
+        public static string GetProductName(string barcode)
+        {  
             return Task.Run(() => Productnaam(barcode)).Result;
         }
 
