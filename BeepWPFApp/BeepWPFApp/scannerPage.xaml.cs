f7c2a65e4c69869f5448a5c60fd213eeed9e528a
Tim Schachtschabel <timschachtschabel14@gmail.com>
2019-12-02 13:37:50 +0100

    Removed scanner page logic

--- BeepWPFApp/BeepWPFApp/scannerPage.xaml.cs
+++ BeepWPFApp/BeepWPFApp/scannerPage.xaml.cs
@@ -15,9 +15,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Emgu.CV;
 using System.Windows.Navigation;
-using PQScan;
+//using PQScan;
 using System.Windows.Shapes;
-using PQScan.BarcodeScanner;
+//using PQScan.BarcodeScanner;
 
 namespace BeepWPFApp
 {
@@ -26,55 +26,6 @@ namespace BeepWPFApp
     /// </summary>
     public partial class scannerPage : Page
     {
-        public scannerPage()
-        {
-            InitializeComponent();
-        }
-        public Bitmap camerafeed()
-        {
-            VideoCapture capture = new VideoCapture();
-            Bitmap image = capture.QueryFrame().Bitmap;
-            Bitmap kaas = new Bitmap(" C:/Users/joepv/Pictures/Camera Roll/jot.jpg" );
-            return kaas;
-        }
-
-        
-
-        public void ScanAllTypeBarcode(Bitmap bmp)
-        {
-            BarcodeResult[] results = BarCodeScanner.Scan(bmp);
-            if (results != null)
-            {
-                foreach (BarcodeResult result in results)
-                {
-                    Console.WriteLine(result.BarType.ToString() + "-" + result.Data);
-                    picturebox.Source = BitmapToImageSource(bmp);
-                    
-                }
-            }
-            else
-            {
-                MessageBox.Show("Niks");
-            }
-        }
-        BitmapImage BitmapToImageSource(Bitmap bitmap)
-        {
-            using (MemoryStream memory = new MemoryStream())
-            {
-                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
-                memory.Position = 0;
-                BitmapImage bitmapimage = new BitmapImage();
-                bitmapimage.BeginInit();
-                bitmapimage.StreamSource = memory;
-                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
-                bitmapimage.EndInit();
-
-                return bitmapimage;
-            }
-        }
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            ScanAllTypeBarcode(camerafeed());
-        }
+      
     }
 }
