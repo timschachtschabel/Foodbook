c52fdceda457a8d3ad965d0609b997b77f85f99e
Mees <meesvanpuijenbroek@gmail.com>
2019-12-03 12:03:28 +0100

    start develop

--- BeepWPFApp/BeepWPFApp/obj/Debug/BeepWPFApp_Content.g.i.cs
+++ BeepWPFApp/BeepWPFApp/obj/Debug/BeepWPFApp_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("telefoon.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("search.png")]
 
 
