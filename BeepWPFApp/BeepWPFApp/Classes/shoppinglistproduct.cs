88f830a20f000621cca361cb896b3a81e16042ca
Tim Schachtschabel <timschachtschabel14@gmail.com>
2020-01-16 13:16:31 +0100

    So guys we did it
    we reached 250000 subscribblers

---
+++ BeepWPFApp/BeepWPFApp/Classes/shoppinglistproduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeepWPFApp.Classes
+{
+    class Shoppinglistproduct
+    {
+        public int Shoppinglist_Id { get; set; }
+        public int User_id { get; set; }
+        public int Product_Id { get; set; }
+        public string Name { get; set; }
+    }
+}
